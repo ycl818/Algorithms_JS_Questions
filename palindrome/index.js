a4a807ff576115e1a2ac824ebbfa767305490246
yichien <kevinyichienlee@gmail.com>
2023-06-10 23:50:56 +0800

    palindrome method 1,2

--- palindrome/index.js
+++ palindrome/index.js
@@ -7,6 +7,14 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {
+//   return str === str.split("").reverse().join("");
+// }
+
+function palindrome(str) {
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
+}
 
 module.exports = palindrome;
