ebc0b5c77cbc3eb64db7cb3fbd9116936d63ef26
yichien <kevinyichienlee@gmail.com>
2023-06-12 23:27:42 +0800

    find max char in the string

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let maxChar = "";
+
+  let max = 0;
+  const charMap = {};
+
+  for (let char of str) {
+    if (!charMap[char]) {
+      charMap[char] = 1;
+    } else {
+      charMap[char]++;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
