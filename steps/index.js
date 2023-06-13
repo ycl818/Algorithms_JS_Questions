741bd6897b74d315ad8ec42afc2489682522ac6e
yichien <kevinyichienlee@gmail.com>
2023-06-13 23:30:50 +0800

    print steps, normal and recursion

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,38 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//   for (let row = 1; row <= n; row++) {
+//     let str = "";
+//     for (let column = 1; column <= n; column++) {
+//       if (column <= row) {
+//         str += "#";
+//       } else {
+//         str += " ";
+//       }
+//     }
+//     console.log(str);
+//   }
+// }
 
+// if (row === n) then we have hit the end of out problem
+// if the 'stair' string has a length === n then we are at the end of a row
+// if the length of the stair string is less than or equal to the row number
+// we're working on, we add a '#', otherwise add a space
+
+function steps(n, row = 0, stair = "") {
+  if (n === row) return;
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1); // new stair default ""
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  steps(n, row, stair);
+}
 module.exports = steps;
