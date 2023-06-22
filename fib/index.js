bbef2cabb983b5c277214b4d2ff05d10fdbea97d
yichien <kevinyichienlee@gmail.com>
2023-06-22 22:19:53 +0800

    fib recursive and loop methods

--- fib/index.js
+++ fib/index.js
@@ -11,12 +11,30 @@
 
 // 4 = 3 2
 
+// function fib(n) {
+//   if (n == 0) return 0;
+//   if (n == 1) return 1;
+//   if (n == 2) return 1;
+//   return fib(n - 1) + fib(n - 2);
+// }
+
 function fib(n) {
-  if (n == 0) return 0;
-  if (n == 1) return 1;
-  if (n == 2) return 1;
+  if (n < 2) return n;
+
   return fib(n - 1) + fib(n - 2);
 }
 
-console.log(fib(10));
+// function fib(n) {
+//   let result = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     let a = result[i - 1];
+//     let b = result[i - 2];
+
+//     result.push(a + b);
+//   }
+
+//   return result[n];
+// }
+
 module.exports = fib;
