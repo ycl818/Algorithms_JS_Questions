62e28ba668ea2eb2f5c0333077533701199dfeae
yichien <kevinyichienlee@gmail.com>
2023-06-23 08:01:43 +0800

    2 stack to create queue

--- qfroms/index.js
+++ qfroms/index.js
@@ -12,8 +12,45 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.firstStack = new Stack();
+    this.secondStack = new Stack();
+  }
+
+  add(record) {
+    this.firstStack.push(record);
+  }
+
+  remove() {
+    while (this.firstStack.peek()) {
+      this.secondStack.push(this.firstStack.pop());
+    }
+
+    const record = this.secondStack.pop();
+
+    while (this.secondStack.peek()) {
+      this.firstStack.push(this.secondStack.pop());
+    }
+
+    return record;
+  }
+
+  peek() {
+    while (this.firstStack.peek()) {
+      this.secondStack.push(this.firstStack.pop());
+    }
+
+    const record = this.secondStack.peek();
+
+    while (this.secondStack.peek()) {
+      this.firstStack.push(this.secondStack.pop());
+    }
+
+    return record;
+  }
+}
 
 module.exports = Queue;
