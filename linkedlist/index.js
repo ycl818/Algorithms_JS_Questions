e0fc31e51ead69813a8dfaaca588386341fb2226
yichien <kevinyichienlee@gmail.com>
2023-06-24 01:02:21 +0800

    most half part of link list

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -2,8 +2,90 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let cnt = 0;
+    let node = this.head;
+    while (node) {
+      node = node.next;
+      cnt++;
+    }
+
+    return cnt;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+    let node = this.head;
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let previous = this.head;
+    let node = this.head.next;
+
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    let curLast = this.getLast();
+    if (curLast) {
+      curLast.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+  }
+}
 
 module.exports = { Node, LinkedList };
